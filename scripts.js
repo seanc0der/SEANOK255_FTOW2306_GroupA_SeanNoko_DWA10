5ac1c23628e7a5820d385e5abc8ace420f11f9b1
seanc0der <sean.noko@gmail.com>
2023-11-07 14:16:35 +0200

    Refactor code logic and encapsulate tally value/state:
    - Streamlined code logic, including element retrieval from the DOM.
    - Enhanced exception handling within the 'getHTML' function to fetch elements by data attribute.
    - Encapsulated the tally value in an object, enabling methods for manipulation (increment/decrement/reset).
    - Added comprehensive JSDoc comments for improved readability and type management.

--- scripts.js
+++ scripts.js
@@ -1,50 +1,80 @@
 // @ts-check
 
 /**
- * The state of the tally app
- * @type {number} */
-let tally = 0;
+ * Finds and returns the HTML element with the specified data attribute.
+ *
+ * @param {object} props - The properties object containing parameters for the element search.
+ * @param {string} props.dataAttr - The data attribute to search for, excluding the `data-` prefix.
+ * @param {string} [props.value] - Optional value assigned to the data attribute, if any.
+ * @returns {HTMLElement} - The HTML element with the specified data attribute.
+ * @throws Throws an error if the element with the specified data attribute is not of type 'HTMLElement'.
+ */
+const getHTML = (props) => {
+	const { dataAttr, value } = props;
+
+	const selector = value
+		? `[data-${dataAttr}]="${value}"`
+		: `[data-${dataAttr}]`;
+
+	const element = document.querySelector(selector);
+
+	if (!(element instanceof HTMLElement)) {
+		throw new Error(`${element} is not an HTMLElement instance.`);
+	}
+
+	return element;
+};
+
+/**
+ * @typedef {Object} Tally - Represents a tally object.
+ * @property {number} value - The current value of the tally counter.
+ * @property {Function} increment - Function to increase the {@link Tally#value} by `1`.
+ * @property {Function} decrement - Function to decrease the {@link Tally#value} by `1`.
+ * @property {Function} reset - Function to reset the {@link Tally#value} to `0`.
+ */
+
+/**
+ * The tally object that keeps track of the tally counter value and provides methods to manipulate it.
+ *
+ * @type {Tally}
+ * */
+const tally = {
+	value: 0,
+
+	increment() {
+		this.value += 1;
+	},
+
+	decrement() {
+		this.value -= 1;
+	},
+
+	reset() {
+		this.value = 0;
+	},
+};
 
 // Query-selecting of elements to be dynamically manipulated
+const elements = {
+	tallyValue: getHTML({ dataAttr: "tally-value" }),
+	minusButton: getHTML({ dataAttr: "minus-button" }),
+	plusButton: getHTML({ dataAttr: "plus-button" }),
+	resetCounter: getHTML({ dataAttr: "reset-counter" }),
+};
 
-/** @type {HTMLElement | null} */
-const tallyCount = document.querySelector("[data-tally-value]");
+// Event handlers
 
-/** @type {HTMLElement | null} */
-const minusButton = document.querySelector("[data-minus-button]");
-
-/** @type {HTMLElement | null} */
-const plusButton = document.querySelector("[data-plus-button]");
-
-/** @type {HTMLElement | null} */
-const resetCounter = document.querySelector("[data-reset-counter]");
-
-// Event handlers and Exception handling
-
-if (!(tallyCount instanceof HTMLElement)) {
-	throw new Error(`${tallyCount} is not an HTMLElement instance.`);
-}
-
-if (!(minusButton instanceof HTMLElement)) {
-	throw new Error(`${minusButton} is not an HTMLElement instance.`);
-}
-minusButton.addEventListener("click", () => {
-	tally -= 1;
-	tallyCount.textContent = tally.toString();
+elements.minusButton.addEventListener("click", () => {
+	tally.decrement();
+	elements.tallyValue.textContent = tally.toString();
 });
 
-if (!(plusButton instanceof HTMLElement)) {
-	throw new Error(`${plusButton} is not an HTMLElement instance.`);
-}
-plusButton.addEventListener("click", () => {
-	tally += 1;
-	tallyCount.textContent = tally.toString();
+elements.plusButton.addEventListener("click", () => {
+	tally.increment();
+	elements.tallyValue.textContent = tally.toString();
 });
 
-if (!(resetCounter instanceof HTMLElement)) {
-	throw new Error(`${resetCounter} is not an HTMLElement instance.`);
-}
-resetCounter.addEventListener("click", () => {
-	tally = 0;
-	tallyCount.textContent = tally.toString();
+elements.resetCounter.addEventListener("click", () => {
+	tally.reset();
+	elements.tallyValue.textContent = tally.toString();
 });
